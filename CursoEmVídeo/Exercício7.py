ddf8fd01768abbad28e10ce490ba37c2fc788bda
Luis Henrique Gomes Zortea <102925017+LuisHZortea@users.noreply.github.com>
2022-12-17 22:46:38 -0300

    Update Exercício7.py

--- CursoEmVídeo/Exercício7.py
+++ CursoEmVídeo/Exercício7.py
@@ -1,5 +1,4 @@
 #Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
-import math
 n1 = float(input("Digite a primeira nota do aluno: "))
 n2 = float(input("Digite a segunda nota do aluno: "))
 m = (n1+n2) / 2
